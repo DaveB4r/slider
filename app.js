1e726e2d9781a3b3faceb8b175cff145cc67bfa1
Juan <davidpinedaaaa@gmail.com>
2023-02-25 06:50:10 -0500

    added play&pause control

--- app.js
+++ app.js
@@ -24,14 +24,18 @@ const sliderControl = num => {
     slider.style.backgroundImage = `url("./images/${num}.jpg")`;
 }
 const sliderPlay = actn =>{
+    let playIcon = document.getElementById('play-icon');
+    let classPlay = "fa-solid fa-";
     if(actn){
         this.runSlider = setInterval(() => {
             if(num == 3) num = 0;
             sliderControl(++num);
         },3000); 
+        playIcon.className = `${classPlay}play`;
         console.log('running');
     }else{
         clearInterval(this.runSlider);
+        playIcon.className = `${classPlay}pause`;
         console.log('paused');
     }
        
